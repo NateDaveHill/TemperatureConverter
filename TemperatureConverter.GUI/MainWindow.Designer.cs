73c70c11207e4e274a7af43553e3c4579c5864fd
Nathan David Hill <hill@software.tirol>
2022-10-04 09:49:45 +0200

    1. rebuilding GUI - zwischenstand

--- TemperatureConverter.GUI/MainWindow.Designer.cs
+++ TemperatureConverter.GUI/MainWindow.Designer.cs
@@ -28,12 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTemperature = new System.Windows.Forms.Label();
+            this.lblTempertureUnit = new System.Windows.Forms.Label();
+            this.lblConvertedTemperature = new System.Windows.Forms.Label();
+            this.lblConvertedUnit = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTemperature
+            // 
+            this.lblTemperature.AutoSize = true;
+            this.lblTemperature.Location = new System.Drawing.Point(32, 33);
+            this.lblTemperature.Name = "lblTemperature";
+            this.lblTemperature.Size = new System.Drawing.Size(96, 20);
+            this.lblTemperature.TabIndex = 0;
+            this.lblTemperature.Text = "Temperature:";
+            // 
+            // lblTempertureUnit
+            // 
+            this.lblTempertureUnit.AutoSize = true;
+            this.lblTempertureUnit.Location = new System.Drawing.Point(209, 33);
+            this.lblTempertureUnit.Name = "lblTempertureUnit";
+            this.lblTempertureUnit.Size = new System.Drawing.Size(52, 20);
+            this.lblTempertureUnit.TabIndex = 1;
+            this.lblTempertureUnit.Text = "temp1";
+            // 
+            // lblConvertedTemperature
+            // 
+            this.lblConvertedTemperature.AutoSize = true;
+            this.lblConvertedTemperature.Location = new System.Drawing.Point(143, 74);
+            this.lblConvertedTemperature.Name = "lblConvertedTemperature";
+            this.lblConvertedTemperature.Size = new System.Drawing.Size(52, 20);
+            this.lblConvertedTemperature.TabIndex = 2;
+            this.lblConvertedTemperature.Text = "temp2";
+            // 
+            // lblConvertedUnit
+            // 
+            this.lblConvertedUnit.AutoSize = true;
+            this.lblConvertedUnit.Location = new System.Drawing.Point(209, 74);
+            this.lblConvertedUnit.Name = "lblConvertedUnit";
+            this.lblConvertedUnit.Size = new System.Drawing.Size(52, 20);
+            this.lblConvertedUnit.TabIndex = 3;
+            this.lblConvertedUnit.Text = "temp3";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(49, 140);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(280, 185);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(69, 129);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(117, 24);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(64, 103);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(117, 24);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(51, 71);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(117, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(40, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(117, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Location = new System.Drawing.Point(133, 33);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(70, 27);
+            this.txtTemperature.TabIndex = 6;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainWindow";
+            this.ClientSize = new System.Drawing.Size(361, 446);
+            this.Controls.Add(this.txtTemperature);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblConvertedUnit);
+            this.Controls.Add(this.lblConvertedTemperature);
+            this.Controls.Add(this.lblTempertureUnit);
+            this.Controls.Add(this.lblTemperature);
+            this.Name = "MainWindow";
+            this.Text = "TemperatureConverter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label lblTemperature;
+        private Label lblTempertureUnit;
+        private Label lblConvertedTemperature;
+        private Label lblConvertedUnit;
+        private GroupBox groupBox1;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
+        private TextBox txtTemperature;
     }
 }
